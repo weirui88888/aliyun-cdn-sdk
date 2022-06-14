771e978a04da8f33aca914e43ce22e6fa11dfabe
weirui88888 <ruiwei88888@163.com>
2022-06-14 18:12:50 +0800

    :construction_worker:debugger again

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function run(): Promise<void> {
       '------------------------------- start -------------------------------'
     )
     core.setOutput('github', github.context.payload)
+    core.setOutput('name', 'jack')
     console.log(
       '------------------------------- end ---------------------------------'
     )
